3eb465b775b52c0a168eb38c9d41b7755d57a103
Kane <fuliang1025@outlook.com>
2017-04-06 22:47:20 +0800

    first execuable simple version

--- money_model.py
+++ money_model.py
@@ -3,11 +3,8 @@ from mesa.time import RandomActivation
 from mesa.space import MultiGrid
 import random
 from mesa.datacollection import DataCollector
-<<<<<<< HEAD
 
-=======
-#master change
->>>>>>> master
+
 def compute_gini(model):
     agent_wealths = [agent.wealth for agent in model.schedule.agents]
     x = sorted(agent_wealths)
